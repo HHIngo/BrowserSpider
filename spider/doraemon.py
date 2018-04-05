d4fe0de07a08e16db62fa818952eb2ff25e77a28
HHIngo <ophadesx@163.com>
2018-04-05 11:09:15 +0800

    1.2

--- spider/doraemon.py
+++ spider/doraemon.py
@@ -1,19 +1,17 @@
-# from pybloom_live import ScalableBloomFilter
-# from bs4 import BeautifulSoup
-# from lxml import etree
+from pybloom_live import ScalableBloomFilter
+from bs4 import BeautifulSoup
+from lxml import etree
 
 
+# 这几个不用可以加个pass，把return和import注释掉
 def scalableBloomFilter():
-    pass
-    # return ScalableBloomFilter()
+    return ScalableBloomFilter()
 
 
 def bs(html):
-    pass
-    # return BeautifulSoup(html, "lxml")
+    return BeautifulSoup(html, "lxml")
 
 
 def get_xpath(html):
-    pass
-    # return etree.HTML(html)
+    return etree.HTML(html)
 
